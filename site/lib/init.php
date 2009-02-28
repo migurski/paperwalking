b6149035e1b3a750db2385e4c4bc4d32b57cd0cd
Michal Migurski <migurski@or.pair.com>
2009-02-27 20:17:05 -0500

    ...

---
+++ site/lib/init.php
@@ -0,0 +1,11 @@
+<?php
+
+    define('DB_DSN', 'mysql://****:****@****/****');
+    define('TMP_DIR', dirname(realpath(__FILE__)).'/../tmp');
+
+    // Amazon S3
+    define('AWS_ACCESS_KEY', '****');
+    define('AWS_SECRET_KEY', '****');
+    define('S3_BUCKET_ID',   '****');
+
+?>

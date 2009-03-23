b00250ba186910d5e59596c857d159570e6196e8
Michal Migurski <migurski@or.pair.com>
2009-03-23 00:43:16 -0400

    Added space for API_PASSWORD in init file

--- site/lib/init.php
+++ site/lib/init.php
@@ -1,7 +1,10 @@
 <?php
 
+    putenv("TZ=America/Los_Angeles");
     define('DB_DSN', 'mysql://****:****@****/****');
     define('TMP_DIR', dirname(realpath(__FILE__)).'/../tmp');
+    
+    define('API_PASSWORD', '****');
 
     // Amazon S3
     define('AWS_ACCESS_KEY', '****');

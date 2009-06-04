49c03af62f2b88c95194014ee6a445f08ae2b41e
Michal Migurski <migurski@or.pair.com>
2009-06-04 15:14:15 -0400

    Added skeletal About page

---
+++ site/templates/about.html.tpl
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>About (Walking Papers)</title>
+	<link rel="stylesheet" href="{$base_dir}/style.css" type="text/css" />
+	<link rel="stylesheet" href="{$base_dir}/index.css" type="text/css" />
+</head>
+<body>
+
+    {include file="navigation.htmlf.tpl"}
+    
+    <h2>About Walking Papers</h2>
+    
+    <p>
+        ...
+    </p>
+
+    {include file="footer.htmlf.tpl"}
+    
+</body>
+</html>
---
+++ site/www/about.php
@@ -0,0 +1,25 @@
+<?php
+
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../lib');
+    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'/usr/home/migurski/pear/lib');
+    require_once 'init.php';
+    require_once 'data.php';
+    
+    $user_id = $_COOKIE['visitor'] ? $_COOKIE['visitor'] : null;
+
+    /**** ... ****/
+    
+    $dbh =& get_db_connection();
+    
+    if($user_id)
+        $user = get_user($dbh, $user_id);
+
+    if($user)
+        setcookie('visitor', $user['id'], time() + 86400 * 31);
+    
+    $sm = get_smarty_instance();
+    
+    header("Content-Type: text/html; charset=UTF-8");
+    print $sm->fetch("about.html.tpl");
+
+?>

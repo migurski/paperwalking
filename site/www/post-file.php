9bf4474c210533e51d38f673a793e5d791f5bc3d
Michal Migurski <migurski@Philippe.(none)>
2009-08-04 12:44:13 -0700

    Seemingly handing both S3 and locally uploaded scans off to the message queue

--- site/www/post-file.php
+++ site/www/post-file.php
@@ -4,9 +4,12 @@
     ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'/usr/home/migurski/pear/lib');
     require_once 'init.php';
     require_once 'data.php';
+    require_once 'Net/URL.php';
     
     $dirname = $_POST['dirname'] ? $_POST['dirname'] : null;
+    $redirect = $_POST['redirect'] ? $_POST['redirect'] : null;
     $expiration = $_POST['expiration'] ? $_POST['expiration'] : null;
+    $file = is_array($_FILES['file']) ? $_FILES['file'] : null;
     
     if(strtotime($expiration) < time())
         die_with_code(401, 'Sorry, expiration date has come and gone');
@@ -17,11 +20,22 @@
     if($posted_signature != $expected_signature)
         die_with_code(401, 'Sorry, bad signature');
     
+    if(is_array($file) && is_uploaded_file($file['tmp_name']))
+    {
+        $object_id = rtrim($dirname, '/').'/'.ltrim($file['name'], '/');
+        $content_bytes = file_get_contents($file['tmp_name']);
     
+        $url = post_file_local($object_id, $content_bytes);
+    }
+
+    $redirect = new Net_URL($redirect);
+    $redirect->addQueryString('url', $url);
+    $redirect = $redirect->getURL();
     
     header('Content-Type: text/plain');
     echo "Thanks, I have no idea what do with a file yet, but thanks.\n";
-    echo "$posted_signature vs $expected_signature\n";
+    echo "$url\n";
+    echo "$redirect\n";
     print_r($_GET);
     print_r($_POST);
     print_r($_FILES);
--- site/www/uploaded.php
+++ site/www/uploaded.php
@@ -5,6 +5,7 @@
     require_once 'init.php';
     require_once 'data.php';
     
+    $url = $_GET['url'] ? $_GET['url'] : null;
     $scan_id = $_GET['scan'] ? $_GET['scan'] : null;
     $object_id = $_GET['key'] ? $_GET['key'] : null;
     $expected_etag = $_GET['etag'] ? $_GET['etag'] : null;
@@ -25,28 +26,40 @@
 
     if($scan && $object_id && $expected_etag)
     {
-        $url = s3_signed_object_url($object_id, time() + 300, 'HEAD');
+        $url = s3_unsigned_object_url($object_id, time() + 300, 'HEAD');
         $etag_match = verify_s3_etag($object_id, $expected_etag);
         
-        if($etag_match && $scan && $scan['last_step'] <= 1)
-        {
-            $dbh->query('START TRANSACTION');
-
-            $added = add_step($dbh, $scan['id'], 1);
-            
-            if($added)
-                add_message($dbh, s3_unsigned_object_url($object_id));
-            
-            $scan = get_scan($dbh, $scan['id']);
-            
-            $dbh->query('COMMIT');
-            
-            //header('Location: http://'.get_domain_name().get_base_dir().'/scan.php?id='.urlencode($scan['id']));
-            //exit();
-        }
-
-        header('Location: http://'.get_domain_name().get_base_dir().'/uploaded.php?scan='.urlencode($scan['id']));
+        $attempted_upload = true;
+        $acceptable_upload = $etag_match;
+        
+    } elseif($scan && $url) {
+        // it's probably fine if a whole URL is being sent over
+        $attempted_upload = true;
+        $acceptable_upload = preg_match('#^http://#', $url);
     }
+    
+    if($attempted_upload && !$acceptable_upload)
+        die_with_code(400, 'Sorry, something about your file was bad');
+
+    if($acceptable_upload && $scan && $scan['last_step'] <= 1)
+    {
+        $dbh->query('START TRANSACTION');
+
+        $added = add_step($dbh, $scan['id'], 1);
+        
+        if($added)
+            add_message($dbh, $url);
+        
+        $scan = get_scan($dbh, $scan['id']);
+        
+        $dbh->query('COMMIT');
+        
+        //header('Location: http://'.get_domain_name().get_base_dir().'/scan.php?id='.urlencode($scan['id']));
+        //exit();
+    }
+
+    if($attempted_upload)
+        header('Location: http://'.get_domain_name().get_base_dir().'/uploaded.php?scan='.urlencode($scan['id']));
     
     $sm = get_smarty_instance();
     $sm->assign('scan', $scan);

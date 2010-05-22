b34e02b471df07789d7f5f817cf80092f4dfc400
migurski <migurski@ubuntu.(none)>
2010-05-22 09:29:46 -0700

    ...

--- decoder/compose.py
+++ decoder/compose.py
@@ -10,19 +10,23 @@ print src
 
 print src.RasterXSize, src.RasterYSize
 
-areas = [(0, 0, src.RasterXSize, 50),
-         (0, 0, 50, src.RasterYSize),
-         (0, src.RasterYSize - 50, src.RasterXSize, 50),
-         (src.RasterXSize - 50, 0, 50, src.RasterYSize)]
+buffer = 25
+
+areas = [(0, 0, src.RasterXSize, buffer),
+         (0, 0, buffer, src.RasterYSize),
+         (0, src.RasterYSize - buffer, src.RasterXSize, buffer),
+         (src.RasterXSize - buffer, 0, buffer, src.RasterYSize)]
 
 def bandaverage(band, x, y, w, h):
     img = PIL.Image.fromstring('L', (w, h), band.ReadRaster(x, y, w, h))
     return PIL.ImageStat.Stat(img).mean[0]
 
 for band in (1, 2, 3):
-    avgs = [(bandaverage(src.GetRasterBand(band), x, y, w, h), w * h)
+    band = src.GetRasterBand(band)
+    avgs = [(bandaverage(band, x, y, w, h), w * h)
             for (x, y, w, h) in areas]
 
-    total = sum( [size for (mean, size) in avgs] )
-    average = sum( [(mean * size / total) for (mean, size) in avgs] )
+    area = sum( [size for (mean, size) in avgs] )
+    average = sum( [(mean * size / area) for (mean, size) in avgs] )
+
     print int(average)

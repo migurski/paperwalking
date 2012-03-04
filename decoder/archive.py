1558d00343d10a78d93312a33d8f54cb7d6f3235
Michal Migurski <migurski@li155-150.members.linode.com>
2012-03-04 06:45:18 +0000

    Improving archive output

--- decoder/archive.py
+++ decoder/archive.py
@@ -1,3 +1,4 @@
+from sys import stderr
 from urllib import urlopen
 from urlparse import urljoin, urlparse
 from os.path import dirname, basename, relpath
@@ -45,6 +46,10 @@ def main(apibase, password, scan_url, index_url, s3_access, s3_secret, bucket_na
     except S3CreateError:
         bucket = conn.get_bucket(bucket_name)
     
+    tile_size = -1
+    
+    print >> stderr, 'Archiving',
+    
     for (index, line) in enumerate(urlopen(index_url)):
         item_url = urljoin(index_url, line.strip())
         
@@ -66,31 +71,44 @@ def main(apibase, password, scan_url, index_url, s3_access, s3_secret, bucket_na
         key = bucket.new_key(item_href)
         key.set_contents_from_string(item_data, headers={'Content-Type': item_type}, policy='public-read')
         
-        print index, item_href
-        
         if index == 0:
+            # we know that the first item is the origin scan
             original_href = item_href
+            print >> stderr, 'original',
         
         elif item_href == 'large.jpg':
             # alphabetically-first preview image
             key = bucket.new_key('--preview.jpg')
             key.set_contents_from_string(item_data, headers={'Content-Type': item_type}, policy='public-read')
+            print >> stderr, 'preview',
         
-        elif tile_pat.match(item_relpath) and 'tile_href' not in locals():
-            tile_href = item_href
+        elif tile_pat.match(item_relpath):
+            if index < 20 and len(item_data) > tile_size:
+                # use the largest of the first few tiles as an example
+                tile_href = item_href
+                tile_size = len(item_data)
+
+            print >> stderr, 't',
         
         elif item_href.endswith('.tif'):
             geotiff_href = item_href
+            print >> stderr, 'geotiff',
         
         elif item_href.endswith('.pdf'):
             print_href = item_href
+            print >> stderr, 'print pdf',
+        
+        else:
+            print >> stderr, '.',
+
+    print >> stderr, '.'
     
     html = html_template % locals()
     
     key = bucket.new_key('index.html')
     key.set_contents_from_string(html, headers={'Content-Type': 'text/html'}, policy='public-read')
     
-    print 'http://www.archive.org/details/%s' % bucket_name
-    print 'http://www.archive.org/download/%s/' % bucket_name
+    print >> stderr, 'http://www.archive.org/details/%s' % bucket_name
+    print >> stderr, 'http://www.archive.org/download/%s/' % bucket_name
     
     yield ALL_FINISHED
